3880f92fc4d453080727e3909aa7a6cf6ea7681f
wildpow <element1028@gmail.com>
2017-09-01 20:21:47 -0700

    chapter 6
    setup Angular,
    setup customer view in Angular
    setup search to reload live

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -15,5 +15,12 @@ class CustomersController < ApplicationController
     else
       @customers = []
     end
+
+    respond_to do |format|
+      format.html {}
+      format.json {
+        render json: {customers: @customers }
+      }
+    end
   end
 end
