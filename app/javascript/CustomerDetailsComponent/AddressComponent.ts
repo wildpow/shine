5a3326a48b2be59b433c7a57ebb31ac6f1adb274
wildpow <element1028@gmail.com>
2017-09-18 09:37:42 -0700

    backend data for customer details view

--- app/javascript/CustomerDetailsComponent/AddressComponent.ts
+++ app/javascript/CustomerDetailsComponent/AddressComponent.ts
@@ -3,10 +3,16 @@ import   template    from "./AddressComponent.html";
 
 var AddressComponent = Component({
   selector: "shine-address",
+  inputs: [
+    "address",
+    "addressType"
+  ],
   template: template
 }).Class({
   constructor: [
     function() {
+      this.address = null;
+      this.addressType = null;
     }
   ]
 });
--- app/javascript/CustomerDetailsComponent/CustomerInfoComponent.ts
+++ app/javascript/CustomerDetailsComponent/CustomerInfoComponent.ts
@@ -3,10 +3,14 @@ import   template    from "./CustomerInfoComponent.html";
 
 var CustomerInfoComponent = Component({
   selector: "shine-customer-info",
+  inputs: [
+    "customer"
+  ],
   template: template
 }).Class({
   constructor: [
     function() {
+      this.customer = null;
     }
   ]
 });
