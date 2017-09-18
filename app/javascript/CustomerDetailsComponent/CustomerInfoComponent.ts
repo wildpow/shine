a2eb501497ebeeb8e0c4c3f13910df4ca1fe9119
wildpow <element1028@gmail.com>
2017-09-17 21:25:10 -0700

    extract parts of view into separate components

---
+++ app/javascript/CustomerDetailsComponent/CustomerInfoComponent.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import   template    from "./CustomerInfoComponent.html";
+
+var CustomerInfoComponent = Component({
+  selector: "shine-customer-info",
+  template: template
+}).Class({
+  constructor: [
+    function() {
+    }
+  ]
+});
+export { CustomerInfoComponent };
